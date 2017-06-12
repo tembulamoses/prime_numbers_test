2adbcb6ec4d2669a8a46457022ab6ec5118f3071
moses <woesites@gmail.com>
2017-06-12 19:21:23 +0300

    last four tests

--- test_generate_prime_numbers.py
+++ test_generate_prime_numbers.py
@@ -25,3 +25,21 @@ class Primenumbers(unittest.TestCase):
 	def test_hundred(self):
 		self.assertEqual(generate_prime_numbers(100),[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
 
+	
+	def test_data_type(self):
+		self.assertEqual(generate_prime_numbers([]), "Only numbers accepted")
+		self.assertEqual(generate_prime_numbers({}), "Only numbers accepted")
+
+	def test_less_than2(self):
+		self.assertEqual(generate_prime_numbers(1), "only numbers greater than 2 accepted")
+		self.assertEqual(generate_prime_numbers(-2), "only numbers greater than 2 accepted")
+		self.assertEqual(generate_prime_numbers(0), "only numbers greater than 2 accepted")
+
+
+	def test_string(self):
+		self.assertEqual(generate_prime_numbers("string"), "Only numbers accepted")
+
+	def test_numbers_correct(self):
+		self.assertEqual(generate_prime_numbers(20), [2,3,5,7,11,13,17,19])
+	
+
